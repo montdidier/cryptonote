47c2800ba8a858297878ff6e0815f0a0a57bddf1
Chris Kruger <chris.kruger@krugerheavyindustries.com>
2018-01-30 14:09:33 +0800

    updated seed nodes

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -105,8 +105,8 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const std::initializer_list<const char*> SEED_NODES = {
-  "127.0.0.1:60147",
-  "127.0.0.1:60147"
+  "35.170.193.217:60147",
+  "207.246.82.112:60147"
 };
 
 struct CheckpointData {
